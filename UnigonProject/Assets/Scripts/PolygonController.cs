8ba4f4c2e3db9f257e309b61aeab9a329bf220d7
AgBallivian <agballivian@miuandes.cl>
2023-11-18 21:10:56 -0600

    Fixed the Angles of spawning

--- UnigonProject/Assets/Scripts/PolygonController.cs
+++ UnigonProject/Assets/Scripts/PolygonController.cs
@@ -11,7 +11,8 @@ public class PolygonController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb.rotation = Random.Range(0f, 360f);
+        // rb.rotation = Random.Range(0f, 360f);
+        // rb.rotation = 90f;
         transform.localScale = Vector3.one * 4f;
     }
 
--- UnigonProject/Assets/Scripts/SpawnerController.cs
+++ UnigonProject/Assets/Scripts/SpawnerController.cs
@@ -13,79 +13,28 @@ public class SpawnerController : MonoBehaviour
     public float spawnDelay = 1f;
     public float shrinkSpeed = 0.7f;
 
-    //Phase Timers
-    private float phasechangeTime = 2.0f;
-    private float phase1Time = 10.0f;
-    private float phase2Time = 20.0f;
-    private float phase3Time = 50.0f;
-    private float phase4Time = 60.0f;
-    private float phase5Time = 75.0f;
-
     private float timer;
     private float globalTimer;
+
+    private List<float> possibleAngles = new List<float>();
+
+    void Start(){
+        int polygonSides = polygonPrefab.GetComponent<PolygonSideGenerator>().sides;
+
+        int sides = Random.Range(minSides, maxSides + 1);
+        float angleStep = 360f / polygonSides; // Use 'sides' instead of 'polygonSides'
+
+        // Create a list of possible angles to spawn the polygon
+        
+        for (int i = 0; i < polygonSides; i++) // Use 'sides' instead of 'polygonSides'
+        {
+            possibleAngles.Add(angleStep * i);
+        }
+    }
 
     void FixedUpdate(){
         globalTimer += Time.deltaTime;
         timer += Time.deltaTime;
-        //Phase 2
-        if (globalTimer >= phase1Time && globalTimer < phase2Time){
-            Debug.Log("Phase 2 " + globalTimer);
-            minSides = 3;
-            maxSides = 5;
-            spawnDelay = 0.9f;
-        }
-        //delay in between phase
-        else if (globalTimer >= phase2Time && globalTimer < phase2Time + phasechangeTime){
-            Debug.Log("Phase 2 delay " + globalTimer);
-            spawnDelay = 100f;
-        }
-        //Phase 3
-        else if (globalTimer >= phase2Time && globalTimer < phase3Time){
-            Debug.Log("Phase 3" + globalTimer);
-            minSides = 2;
-            maxSides = 4;
-            spawnDelay = 0.6f;
-            shrinkSpeed = 1.1f;
-        }
-        //Delay in between phases
-        else if (globalTimer >= phase3Time && globalTimer < phase3Time + phasechangeTime){
-            Debug.Log("Phase 3 delay" + globalTimer);
-            spawnDelay = 100f;
-        }
-        //Phase 4
-        else if (globalTimer >= phase3Time && globalTimer < phase4Time){
-            Debug.Log("Phase 4" + globalTimer);
-            minSides = 5;
-            maxSides = 5;
-            spawnDelay = 1.1f;
-            shrinkSpeed = 1.7f;
-        }
-        //Delay in between phases
-        else if (globalTimer >= phase4Time && globalTimer < phase4Time + phasechangeTime){
-            Debug.Log("Phase 4 delay" + globalTimer);
-            spawnDelay = 100f;
-        }
-        //Phase 5
-        else if (globalTimer >= phase4Time && globalTimer < phase5Time){
-            Debug.Log("Phase 5" + globalTimer);
-            minSides = 1;
-            maxSides = 5;
-            spawnDelay = 0.55f;
-            shrinkSpeed = 1.2f;
-        }
-        //Delay in Between Phases
-        else if (globalTimer >= phase5Time && globalTimer < phase5Time + phasechangeTime){
-            Debug.Log("Phase 5 delay" + globalTimer);
-            spawnDelay = 100f;
-        }
-        //Phase 6
-        else if (globalTimer >= phase5Time){
-            Debug.Log("Phase 6" + globalTimer);
-            minSides = 3;
-            maxSides = 5;
-            spawnDelay = 0.5f;
-            shrinkSpeed = 1.7f;
-        }
 
         if(timer >= spawnDelay){
             SpawnObjects();
@@ -93,33 +42,21 @@ public class SpawnerController : MonoBehaviour
         }
     }
 
-    private void SpawnObjects(){
-        int polygonSides = polygonPrefab.GetComponent<PolygonSideGenerator>().sides;
-
+    private void SpawnObjects()
+    {
         int sides = Random.Range(minSides, maxSides + 1);
-        float angleStep = 360f / polygonSides;
-
-        // Create a list of possible angles to spawn the polygon
-        List<float> possibleAngles = new List<float>();
-        for (int i = 0; i < polygonSides; i++)
-        {
-            possibleAngles.Add(angleStep * i);
-        }
-
         // Choose a random angle from the list
         int randomAngleIndex = Random.Range(0, possibleAngles.Count);
         float spawnAngle = possibleAngles[randomAngleIndex];
 
-        // Rotate around z axis
-        // Vector3 spawnAngleV = new Vector3(0f, 0f, spawnAngle);
-        // Quaternion spawnAngleQ = Quaternion.Euler(spawnAngleV);
+        // Rotate around z-axis
+        Quaternion spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle);
 
-        // Debug.Log(spawnAngleQ);
+        Debug.Log(spawnAngle);
 
         Vector3 spawnPosition = new Vector3(0f, -3f, 0f);
 
-        GameObject spawnedPolygon = Instantiate(polygonPrefab, spawnPosition, Quaternion.identity);
-        spawnedPolygon.transform.RotateAround(spawnPosition, Vector3.forward, spawnAngle);
+        GameObject spawnedPolygon = Instantiate(polygonPrefab, spawnPosition, spawnRotation);
         spawnedPolygon.GetComponent<PolygonSideGenerator>().sidesToCreate = sides;
         spawnedPolygon.GetComponent<PolygonController>().shrinkSpeed = shrinkSpeed;
     }
