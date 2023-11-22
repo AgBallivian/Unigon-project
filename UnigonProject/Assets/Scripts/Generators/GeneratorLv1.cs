65181448314ca15a7a866c563310958d7d12adca
agballivian <agballivian@miuandes.cl>
2023-11-22 18:11:39 -0500

    Added Patterns

--- UnigonProject/Assets/Scripts/Generators/GeneratorLv1.cs
+++ UnigonProject/Assets/Scripts/Generators/GeneratorLv1.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GeneratorLv1 : MonoBehaviour
 {
     public GameObject polygonPrefab;
-    private float patternchangeTime = 5.0f;
+    public float patternchangeTime = 3.0f;
+    public float patternSpeedTime = 0.5f;
 
     //temporal Randomess (ADD PATTERNS NEXT UPDATE)
     // public float spawnDelay = 1f;
@@ -32,7 +33,18 @@ public class GeneratorLv1 : MonoBehaviour
         timer += Time.deltaTime;
 
         if(timer >= patternchangeTime){
-            StartCoroutine(Pattern3C());
+            //TODO: Optimizar le elecion random de pattrones (?)
+            int randomPattern = Random.Range(0, 3);
+            //Cambiarlo a un switch case
+            if(randomPattern == 0){
+                // Debug.Log("Spawning 111 ; Time: " + patternchangeTime);
+                StartCoroutine(patt_2on2());
+            }
+            if(randomPattern > 0){
+                // Debug.Log("Spawning 3c ; Time: " + patternchangeTime);
+                StartCoroutine(patt_3c());
+            }
+            
             timer = 0;
         }
 
@@ -46,7 +58,7 @@ public class GeneratorLv1 : MonoBehaviour
         // Quaternion spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle);
 
     //Patter 3-C
-    private IEnumerator Pattern3C()
+    private IEnumerator patt_3c()
     {
         // Choose a random angle from the list
         int randomAngleIndex = Random.Range(0, possibleAngles.Count);
@@ -56,21 +68,97 @@ public class GeneratorLv1 : MonoBehaviour
 
         //Spawn wall 1 of 3
         SpawnWall(polygonSides, polygonSides - 1, spawnRotation);
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(patternSpeedTime);
         //Spawn wall 2 of 3
         Quaternion spawnRotation2 = Quaternion.Euler(0f, 0f, spawnAngle + 90f);
         SpawnWall(polygonSides, polygonSides - 1, spawnRotation2);
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(patternSpeedTime);
         //Spawn wall 3 of 3
         Quaternion spawnRotation3 = Quaternion.Euler(0f, 0f, spawnAngle);
         SpawnWall(polygonSides, polygonSides - 1, spawnRotation3);
+        yield return new WaitForSeconds(patternSpeedTime);
 
+        //DELAY SO PATTERNS DON'T OVERLAP   
+        //TODO: OPTIMIZAR ESTA WEA
+        yield return new WaitForSeconds(patternchangeTime);
+    }
+
+    //Pattern lane2-2-2
+    private IEnumerator patt_111()
+    {
+        int randomAngleIndex = Random.Range(0, possibleAngles.Count);
+        float spawnAngle = possibleAngles[randomAngleIndex];
+        // Rotate around z-axis
+        Quaternion spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle); 
+        //1on1 1
+        SpawnWall(polygonSides, 1, spawnRotation);
+        spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle+90f); 
+        SpawnWall(polygonSides, 1, spawnRotation);
+        yield return new WaitForSeconds(patternSpeedTime);
+
+        //Next angle on list 
+        //TODO: OPTIMIZA ESTO
+        int indx = randomAngleIndex + 1;
+        if (indx > possibleAngles.Count){
+            indx = 0;
+        }
+        float spawnAngle2 = possibleAngles[indx];
+        Quaternion spawnRotation2 = Quaternion.Euler(0f, 0f, spawnAngle2);
+        SpawnWall(polygonSides, 1, spawnRotation2);
+        spawnRotation2 = Quaternion.Euler(0f, 0f, spawnAngle2+90f);
+        SpawnWall(polygonSides, 1, spawnRotation2);
+        yield return new WaitForSeconds(patternSpeedTime);
+
+        //Next angle on list 
+        //TODO: OPTIMIZA ESTO
+        indx = indx + 1;
+        if (indx > possibleAngles.Count){
+            indx = 0;
+        }
+        float spawnAngle3 = possibleAngles[indx];
+        Quaternion spawnRotation3 = Quaternion.Euler(0f, 0f, spawnAngle3);
+        SpawnWall(polygonSides, 1, spawnRotation3);
+        spawnRotation3 = Quaternion.Euler(0f, 0f, spawnAngle3+90f);
+        SpawnWall(polygonSides, 1, spawnRotation3);
+        yield return new WaitForSeconds(patternSpeedTime);
+
+        //DELAY SO PATTERNS DON'T OVERLAP   
+        //TODO: OPTIMIZAR ESTA WEA
+        yield return new WaitForSeconds(patternchangeTime);
+    }
+    
+    private IEnumerator patt_2on2()
+    {
+        //TODO: FIX THIS FOR, ITS NOT SPAWNING THE WALLS IN THE NEXT AVAILABLE ANGLE
+        int randomAngleIndex = Random.Range(0, possibleAngles.Count);
+        float spawnAngle = possibleAngles[randomAngleIndex];
+        // Rotate around z-axis
+        Quaternion spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle); 
+        int indx = randomAngleIndex;
+        for(int i = 0; i<polygonSides; i++){
+            Debug.Log("spawn angle: " + spawnAngle + " ; i: " + i);
+            spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle); 
+            SpawnWall(polygonSides, 1, spawnRotation);
+            spawnRotation = Quaternion.Euler(0f, 0f, spawnAngle+90f); 
+            SpawnWall(polygonSides, 1, spawnRotation);
+            yield return new WaitForSeconds(0.7f);
+            
+            indx = indx + 1; 
+            if (indx >= possibleAngles.Count){
+                indx = 0;
+            }
+            
+            spawnAngle = possibleAngles[indx];
+        }
+        //DELAY SO PATTERNS DON'T OVERLAP   
+        //TODO: OPTIMIZAR ESTA WEA
+        yield return new WaitForSeconds(patternchangeTime);
     }
 
     //Spawn the wall function 
     private void SpawnWall(int sides, int sidesToCreate, Quaternion spawnRotation)
     {
-        Debug.Log("Angle of spawn: " + spawnRotation.eulerAngles.z);
+        // Debug.Log("Angle of spawn: " + spawnRotation.eulerAngles.z);
         Vector3 spawnPosition = new Vector3(0f, -3f, 0f);
         GameObject spawnedPolygon = Instantiate(polygonPrefab, spawnPosition, spawnRotation);
         spawnedPolygon.GetComponent<PolygonSideGenerator>().sides = sides;
